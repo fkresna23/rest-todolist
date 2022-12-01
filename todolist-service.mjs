2434ecbb6f9321307653eaa8ed75f4ee4d9ea528
f.kresna23 <f.kresna23@gmail.com>
2022-12-01 13:39:58 +0700

    API Update Todolist

--- todolist-service.mjs
+++ todolist-service.mjs
@@ -30,4 +30,16 @@ export class TodolistService {
             response.end();
         })
     }
+
+    updateTodo(request,response){
+        request.addListener("data", (data) => {
+            const body = JSON.parse(data.toString());
+            if (this.todolist[body.id]){
+                this.todolist[body.id] = body.todo;
+            }
+
+            response.write(this.getJsonTodoList());
+            response.end();
+        })
+    }
 }
