5aa270e5893090f6df8c7749eabeaba2f3a5fbb5
f.kresna23 <f.kresna23@gmail.com>
2022-12-01 13:34:58 +0700

    API Create Todolist

--- app.mjs
+++ app.mjs
@@ -7,11 +7,12 @@ const server = http.createServer((request, response) => {
 
     response.setHeader("Content-Type", "application/json");
 
-    if(request.method === "GET"){
+    if (request.method === "GET"){
         service.getTodoList(request,response);
+    } else if (request.method === "POST") {
+        service.createTodo(request,response);
     }
-    // response.write("Todolist API");
-    // response.end;
+
 });
 
 server.listen(3000)
